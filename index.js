7372e17288da6748e9e955c25036889270683230
kaushikar11 <kaushikar2211@gmail.com>
2024-10-10 10:34:54 +0530

    cors v3

--- index.js
+++ index.js
@@ -5,7 +5,6 @@ const cors = require('cors');
 const studentRoutes = require('./routes/studentRoutes');
 const adminRoutes = require('./routes/adminRoutes');
 const leavingFormRoutes = require('./routes/LeavingFormRoutes');
-// const complaintRoutes = require('./routes/complaintRoutes');
 
 const CLIENT_URL = process.env.CLIENT_URL || 'http://localhost:3000';
 
@@ -14,44 +13,25 @@ const PORT = 3031;
 
 app.use(express.json());
 
-
-const allowCors = fn => async (req, res) => {
-    res.setHeader('Access-Control-Allow-Credentials', true)
-    res.setHeader('Access-Control-Allow-Origin', '*')
-    // another common pattern
-    // res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
-    res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT')
-    res.setHeader(
-      'Access-Control-Allow-Headers',
-      'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
-    )
-    if (req.method === 'OPTIONS') {
-      res.status(200).end()
-      return
-    }
-    return await fn(req, res)
-  }
-  
-  const handler = (req, res) => {
-    const d = new Date()
-    res.end(d.toString())
-  }
-  
-  module.exports = allowCors(handler)
-
+// CORS options
 const corsOptions = {
-    origin: CLIENT_URL, // Only allow this origin
+    origin: CLIENT_URL, // Only allow the specified origin
+    credentials: true,  // Allow cookies and credentials
     optionsSuccessStatus: 200, // Response for successful OPTIONS requests
 };
+
+// Use CORS middleware
 app.use(cors(corsOptions));
 
 // Connect to MongoDB
 mongoose.connect(process.env.MONGODB_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
-}).then(() => {
+})
+.then(() => {
     console.log("Connected successfully to MongoDB");
-}).catch((error) => {
+})
+.catch((error) => {
     console.error("Error connecting to MongoDB:", error);
 });
 
@@ -59,7 +39,6 @@ mongoose.connect(process.env.MONGODB_URI, {
 app.use('/api/students', studentRoutes);
 app.use('/api/admin', adminRoutes);
 app.use('/api/leaving-forms', leavingFormRoutes);
-// app.use('/api/complaints', complaintRoutes);
 
 // Start the server
 app.listen(PORT, () => {
